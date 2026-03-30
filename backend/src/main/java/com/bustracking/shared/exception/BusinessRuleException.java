f339337cebaf0591e7f6fbfb2eeba6a4766f64e9
Alvaro499 <alvarosiles499@gmail.com>
2026-03-30 16:02:48 -0600

    New Constructor For General Message (For Devs and Users)

--- backend/src/main/java/com/bustracking/shared/exception/BusinessRuleException.java
+++ backend/src/main/java/com/bustracking/shared/exception/BusinessRuleException.java
@@ -4,4 +4,8 @@ public class BusinessRuleException extends ApplicationException {
     public BusinessRuleException(ErrorCode errorCode, String userMessage, String devMessage) {
         super(errorCode, userMessage, devMessage);
     }
+
+    public BusinessRuleException(ErrorCode errorCode, String generalMessage) {
+        super(errorCode, generalMessage, generalMessage);
+    }
 }
