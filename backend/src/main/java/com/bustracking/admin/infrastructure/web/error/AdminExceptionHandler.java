7b1a59b6dbaee174aedd8874cd4a0b895fdad6cc
Alvaro499 <alvarosiles499@gmail.com>
2026-04-05 22:03:16 -0600

    Unused import deleted

--- backend/src/main/java/com/bustracking/admin/infrastructure/web/error/AdminExceptionHandler.java
+++ backend/src/main/java/com/bustracking/admin/infrastructure/web/error/AdminExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.bustracking.shared.exception.ApplicationException;
 import com.bustracking.shared.exception.BusinessRuleException;
 import com.bustracking.shared.exception.ExternalServiceException;
 import com.bustracking.shared.exception.NotFoundException;
