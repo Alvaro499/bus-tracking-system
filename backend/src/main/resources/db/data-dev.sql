63e0a06b59f41573f97e7d0a16a30e0e0e3c5dac
Alvaro499 <alvarosiles499@gmail.com>
2026-04-27 23:09:06 -0600

    data-dev.sql is idempotent

--- backend/src/main/resources/db/data-dev.sql
+++ backend/src/main/resources/db/data-dev.sql
@@ -1,3 +1,4 @@
-
+-- data-dev.sql
+DELETE FROM tracking.bus_location;
 INSERT INTO tracking.bus_location(bus_id,lat,lng, updated_at)
 VALUES ('650e8400-e29b-41d4-a716-446655440001', 9.934739, -84.087502, NOW());
