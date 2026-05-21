87e6e135850a94347135e23412862b122d5d29c5
Alvaro499 <alvarosiles499@gmail.com>
2026-05-20 23:58:01 -0600

    add @Transactional annotation fot Test Flows

--- backend/src/test/java/com/bustracking/shared/testinfrastructure/FlowIntegrationTest.java
+++ backend/src/test/java/com/bustracking/shared/testinfrastructure/FlowIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -19,6 +20,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @AutoConfigureMockMvc
 @Testcontainers
 @ActiveProfiles("test")
+@Transactional
 public abstract class FlowIntegrationTest {
 
     @Container
