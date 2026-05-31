08fd54c036b3cfce690db9fcbeab1f959bc37764
Alvaro499 <alvarosiles499@gmail.com>
2026-05-30 21:09:02 -0600

    wrong url corrected

--- frontend/management-app/src/app/driver/trip-detail-.tsx/page.tsx
+++
@@ -1,6 +0,0 @@
-import { TripDetailContainer } from '../../../features/driver/trip-detail.container';
-
-export default function TripDetailPage() {
-    const mockTripId = 'trip-123';
-      return <TripDetailContainer tripId={mockTripId} />;
-}
