fe39565d10b97c22bc5002938409fff7b50a1bda
Alvaro499 <alvarosiles499@gmail.com>
2026-07-15 23:51:52 -0600

    new: FinishTripUseCase incomplete

---
+++ backend/src/main/java/com/bustracking/tracking/application/usecase/FinishTripUseCase.java
@@ -0,0 +1,19 @@
+package com.bustracking.tracking.application.usecase;
+
+import java.util.UUID;
+
+import com.bustracking.tracking.domain.model.TripDetailView;
+
+public class FinishTripUseCase {
+
+    public TripDetailView execute(UUID tripId) {
+
+        // We don't need to verifiy if bus exists (new future service domain to recycle business logic)
+
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+
+    
+}
